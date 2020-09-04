4dd3c681a654f55679ee68f23e536d66914a1b43
ramakanth35hi <rk.kanth39@gmail.com>
2020-09-04 15:32:34 +0530

    chore: Removed SUB_DIR

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ echo "[default]
 aws_access_key_id = ${SPACE_ACCESS_KEY_ID}
 aws_secret_access_key = ${SPACE_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 sync ${SOURCE_DIR} s3://${SPACE_NAME}/${SUB_DIR} \
+aws s3 sync ${SOURCE_DIR} s3://${SPACE_NAME} \
             --follow-symlinks \
             --endpoint https://${SPACE_REGION}.digitaloceanspaces.com $*
 
