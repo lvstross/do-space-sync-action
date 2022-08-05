8685a1d60b73efd78b117644d816a8819b767895
Levi Gonzales <lvstross@gmail.com>
2022-08-05 08:18:24 -0600

    added support for remote path definition

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,6 @@ aws_secret_access_key = ${SPACE_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 aws s3 sync ${SOURCE_DIR} s3://${SPACE_NAME} \
             --follow-symlinks \
-            --endpoint https://${SPACE_REGION}.digitaloceanspaces.com $*
+            --endpoint https://${SPACE_REGION}.digitaloceanspaces.com/${REMOTE_PATH} $*
 
 rm -rf ~/.aws
