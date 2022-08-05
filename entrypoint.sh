17d207b0448feb76712bd49e32f3bd9d4b60b744
Levi Gonzales <lvstross@gmail.com>
2022-08-05 08:27:52 -0600

    fixed remote path usage

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ echo "[default]
 aws_access_key_id = ${SPACE_ACCESS_KEY_ID}
 aws_secret_access_key = ${SPACE_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 sync ${SOURCE_DIR} s3://${SPACE_NAME} \
+aws s3 sync ${SOURCE_DIR} s3://${SPACE_NAME}${REMOTE_PATH} \
             --follow-symlinks \
-            --endpoint https://${SPACE_REGION}.digitaloceanspaces.com/${REMOTE_PATH} $*
+            --endpoint https://${SPACE_REGION}.digitaloceanspaces.com/ $*
 
 rm -rf ~/.aws
